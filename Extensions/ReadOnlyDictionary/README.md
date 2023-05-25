bb28daaaa6a5356b5bdf877ba207dc52c28d060d
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2023-05-25 22:48:21 +0200

    Added ReadOnlyDictionary extension

---
+++ Extensions/ReadOnlyDictionary/README.md
@@ -0,0 +1,12 @@
+﻿# ReadOnlyDictionary
+
+Extension allows to convert `Dictionary` to `ReadOnlyDictionary` especially for `Dictionary` with different types of `Collection`-s which are converted to `IReadOnlyCollection`.
+
+## v1.0.0
+Supports:
+* `IDictionary<TKey, IList<TValue>>`
+* `IDictionary<TKey, IEnumerable<TValue>>`
+* `IDictionary<TKey, ICollection<TValue>>`
+* `IDictionary<TKey, HashSet<TValue>>`
+* `IDictionary<TKey, Collection<TValue>>`
+* `IDictionary<TKey, List<TValue>>`
---
+++ Extensions/ReadOnlyDictionary/ReadOnlyDictionaryExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Extensions.ReadOnlyDictionary
+{
+    public static class ReadOnlyDictionaryExtension
+    {
+        public static IReadOnlyDictionary<TKey, IReadOnlyCollection<TValue>> AsReadOnly<TKey, TValue>(this IDictionary<TKey, List<TValue>> input)
+            where TKey : notnull
+        {
+            var readOnly = new Dictionary<TKey, IReadOnlyCollection<TValue>>();
+
+            foreach (var entry in input ?? throw new ArgumentNullException())
+            {
+                readOnly.Add(entry.Key, new ReadOnlyCollection<TValue>(entry.Value.AsReadOnly()));
+            }
+
+            return new ReadOnlyDictionary<TKey, IReadOnlyCollection<TValue>>(readOnly);
+        }
+
+        public static IReadOnlyDictionary<TKey, IReadOnlyCollection<TValue>> AsReadOnly<TKey, TValue>(this IDictionary<TKey, IList<TValue>> input) where TKey : notnull => AsReadOnly(input.ToDictionary(x => x.Key, x => x.Value.ToList()));
+        public static IReadOnlyDictionary<TKey, IReadOnlyCollection<TValue>> AsReadOnly<TKey, TValue>(this IDictionary<TKey, IEnumerable<TValue>> input) where TKey : notnull => AsReadOnly(input.ToDictionary(x => x.Key, x => x.Value.ToList()));
+        public static IReadOnlyDictionary<TKey, IReadOnlyCollection<TValue>> AsReadOnly<TKey, TValue>(this IDictionary<TKey, ICollection<TValue>> input) where TKey : notnull => AsReadOnly(input.ToDictionary(x => x.Key, x => x.Value.ToList()));
+        public static IReadOnlyDictionary<TKey, IReadOnlyCollection<TValue>> AsReadOnly<TKey, TValue>(this IDictionary<TKey, HashSet<TValue>> input) where TKey : notnull => AsReadOnly(input.ToDictionary(x => x.Key, x => x.Value.ToList()));
+        public static IReadOnlyDictionary<TKey, IReadOnlyCollection<TValue>> AsReadOnly<TKey, TValue>(this IDictionary<TKey, Collection<TValue>> input) where TKey : notnull => AsReadOnly(input.ToDictionary(x => x.Key, x => x.Value.ToList()));
+    }
+}
