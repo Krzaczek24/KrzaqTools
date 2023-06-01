32df02408f54aa4c5e18e37b645f18527626b176
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2023-06-01 22:57:06 +0200

    Added EnumExtension library

---
+++ Extensions/Enum/EnumExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace KrzaqTools.EnumExtension
+{
+    public static class EnumExtension
+    {
+        public static string? GetDescription(this Enum @enum)
+        {
+            return @enum.GetType().GetField(@enum.ToString()).GetCustomAttribute<DescriptionAttribute>()?.Description;
+        }
+    }
+}
---
+++ Extensions/Enum/README.md
@@ -0,0 +1,5 @@
+﻿# EnumExtension
+Extension that helps working with enums.
+
+## v1.0.0
+* Added `string? GetDescription(this Enum @enum)`
