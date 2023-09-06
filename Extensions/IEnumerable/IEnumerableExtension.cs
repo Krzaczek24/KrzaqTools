832fe77e967840f382bc080e346352f3dc4e6923
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2023-09-06 16:26:22 +0200

    Added ToReadOnlyDictionary method

--- Extensions/IEnumerable/IEnumerableExtension.cs
+++ Extensions/IEnumerable/IEnumerableExtension.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace KrzaqTools.Extensions
@@ -7,5 +9,10 @@ namespace KrzaqTools.Extensions
     {
         public static bool HasAny<T>(this IEnumerable<T> first, IEnumerable<T> second) => first.Intersect(second).Any();
         public static bool HasAll<T>(this IEnumerable<T> first, IEnumerable<T> second) => first.Intersect(second).Count() == second.Count();
+
+        public static IReadOnlyDictionary<TKey, TValue> ToReadOnlyDictionary<T, TKey, TValue>(this IEnumerable<T> first, Func<T, TKey> keySelector, Func<T, TValue> valueSelector)
+        {
+            return new ReadOnlyDictionary<TKey, TValue>(first.ToDictionary(keySelector, valueSelector));
+        }
     }
 }
--- Extensions/IEnumerable/README.md
+++ Extensions/IEnumerable/README.md
@@ -1,6 +1,10 @@
 ﻿# Krzaq.Extensions.IEnumerable
 Extension allows to convert `Dictionary` to `ReadOnlyDictionary` especially for `Dictionary` with different types of `Collection`-s which are converted to `IReadOnlyCollection`.
 
+## v1.1.0
+Added:
+* `IReadOnlyDictionary<TKey, TValue> ToReadOnlyDictionary<T, TKey, TValue>(this IEnumerable<T> first, Func<T, TKey> keySelector, Func<T, TValue> valueSelector)`
+
 ## v1.0.0
 Supports:
 * `bool HasAny<T>(this IEnumerable<T> first, IEnumerable<T> second)`
