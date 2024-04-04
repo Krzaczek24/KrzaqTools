a8fdf8788137e65ae8ed1d6a1c93f798de0b72a6
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2024-04-05 01:37:37 +0200

    Added: Scale, CutOff, ScaleAndCutOff and Threshold methods

--- Extensions/INumber/INumberExtension.cs
+++ Extensions/INumber/INumberExtension.cs
@@ -13,6 +13,28 @@ namespace KrzaqTools.Extensions
                 && number.IsLesserThan(max, inclusivity.HasFlag(Inclusivity.Right));
         }
 
+        public static T Scale<T>(this T value, T minSourceValue, T maxSourceValue, T minTargetValue, T maxTargetValue) where T : INumber<T>
+        {
+            return minTargetValue + (value - minSourceValue) / (maxSourceValue - minSourceValue) * (maxTargetValue - minTargetValue);
+        }
+
+        public static T CutOff<T>(this T value, T min, T max) where T : INumber<T>
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
+
+        public static T ScaleAndCutOff<T>(this T value, T minSourceValue, T maxSourceValue, T minTargetValue, T maxTargetValue) where T : INumber<T>
+        {
+            return value.Scale(minSourceValue, maxSourceValue, minTargetValue, maxTargetValue).CutOff(minTargetValue, maxTargetValue);
+        }
+
+        public static T Threshold<T>(this T value, T threshold) where T : INumber<T>
+        {
+            return T.Abs(value) >= threshold ? value : T.Zero;
+        }
+
         private static bool IsLesserThan<T>(this T number, T value, bool inclusive)
             where T : INumber<T> => number < value || inclusive && number == value;
 
--- Extensions/INumber/README.md
+++ Extensions/INumber/README.md
@@ -1,6 +1,13 @@
 ﻿# Krzaq.Extensions.INumber
 Extension adds few method to `INumber` types.
 
+## v1.1.0
+Added:
+* `T Scale<T>(this T value, T minSourceValue, T maxSourceValue, T minTargetValue, T maxTargetValue)`
+* `T CutOff<T>(this T value, T min, T max)`
+* `T ScaleAndCutOff<T>(this T value, T minSourceValue, T maxSourceValue, T minTargetValue, T maxTargetValue)`
+* `T Threshold<T>(this T value, T threshold)`
+
 ## v1.0.9
 * NuGet package reference fix
 
