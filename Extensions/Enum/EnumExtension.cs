7228ea81c1b2e0a85164f19e6e06302dd77c06dd
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2024-06-13 20:56:33 +0200

    Added: GetAttribute, GetAttributes and GetAttributePropertyValues methods

--- Extensions/Enum/EnumExtension.cs
+++ Extensions/Enum/EnumExtension.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
 
 namespace KrzaqTools.Extensions
 {
     public static class EnumExtension
     {
+        public static TAttribute? GetAttribute<TAttribute>(this Enum @enum) where TAttribute : Attribute
+        {
+            var attribute = @enum.GetType().GetField(@enum.ToString()).GetCustomAttribute<TAttribute>();
+            return attribute;
+        }
+
+        public static IEnumerable<TAttribute> GetAttributes<TAttribute>(this Enum @enum) where TAttribute : Attribute
+        {
+            var attributes = @enum.GetType().GetField(@enum.ToString()).GetCustomAttributes<TAttribute>();
+            return attributes;
+        }
+
         public static string? GetDescription(this Enum @enum)
         {
             _ = TryGetAttributePropertyValue(@enum, (DescriptionAttribute attr) => attr.Description, out string descripton);
@@ -15,10 +29,18 @@ namespace KrzaqTools.Extensions
         public static bool TryGetAttributePropertyValue<TAttribute, TProperty>(this Enum @enum, Func<TAttribute, TProperty> selector, out TProperty property)
             where TAttribute : Attribute
         {
-            var attribute = @enum.GetType().GetField(@enum.ToString()).GetCustomAttribute<TAttribute>();
+            var attribute = GetAttribute<TAttribute>(@enum);
             bool found = attribute != null;
             property = found ? selector(attribute!) : default!;
             return found;
+        }
+
+        public static IEnumerable<TProperty> GetAttributePropertyValues<TAttribute, TProperty>(this Enum @enum, Func<TAttribute, TProperty> selector)
+            where TAttribute : Attribute
+        {
+            var attributes = GetAttributes<TAttribute>(@enum);
+            var properties = attributes.Select(selector);
+            return properties;
         }
     }
 }
--- Extensions/Enum/README.md
+++ Extensions/Enum/README.md
@@ -1,6 +1,11 @@
 ﻿# Krzaq.Extensions.Enum
 Extension that helps working with enums.
 
+## v1.1.1
+* Added `TAttribute? GetAttribute<TAttribute>(this Enum @enum)`
+* Added `IEnumerable<TAttribute> GetAttributes<TAttribute>(this Enum @enum)`
+* Added `IEnumerable<TProperty> GetAttributePropertyValues<TAttribute, TProperty>(this Enum @enum, Func<TAttribute, TProperty> selector)`
+
 ## v1.1.0
 * Removed `TProperty GetAttributePropertyValue<TAttribute, TProperty>(this Enum @enum, Func<TAttribute, TProperty> selector)`
 * Added `bool TryGetAttributePropertyValue<TAttribute, TProperty>(this Enum @enum, Func<TAttribute, TProperty> selector, TProperty property)`
