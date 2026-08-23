9368f618acb5654a8a938ea38f9b3369f576b3db
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2026-08-23 23:31:02 +0200

    Fixed GetMemberName method

--- Extensions/Expression/ExpressionExtension.cs
+++ Extensions/Expression/ExpressionExtension.cs
@@ -5,7 +5,7 @@ namespace Krzaq.Extensions.Expression
 {
     public static class ExpressionExtension
     {
-        public static string GetMemberName<T>(this Expression<Func<T, object>> expression) => expression.Body switch
+        public static string GetMemberName<T>(this LambdaExpression expression) => expression.Body switch
         {
             MemberExpression memberExpression => memberExpression.Member.Name,
             UnaryExpression unaryExpression when unaryExpression.Operand is MemberExpression memberOperand => memberOperand.Member.Name,
--- Extensions/Expression/README.md
+++ Extensions/Expression/README.md
@@ -1,6 +1,10 @@
 ﻿# Krzaq.Extensions.Expression
 Extension adds few method to Linq expressions.
 
+## v1.0.1
+Changed:
+* `string GetMemberName<T>(this Expression<Func<T, object>> expression)` to `string GetMemberName<T>(this LambdaExpression expression)`
+
 ## v1.0.0
 Added:
 * `string GetMemberName<T>(this Expression<Func<T, object>> expression)`
