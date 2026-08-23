f344945673bacabf80a0286265ae00579dc5ec56
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2026-08-23 23:22:06 +0200

    Added Krzaq.Extensions.Expression

---
+++ Extensions/Expression/ExpressionExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Krzaq.Extensions.Expression
+{
+    public static class ExpressionExtension
+    {
+        public static string GetMemberName<T>(this Expression<Func<T, object>> expression) => expression.Body switch
+        {
+            MemberExpression memberExpression => memberExpression.Member.Name,
+            UnaryExpression unaryExpression when unaryExpression.Operand is MemberExpression memberOperand => memberOperand.Member.Name,
+            _ => throw new ArgumentException("Invalid expression")
+        };
+    }
+}
---
+++ Extensions/Expression/README.md
@@ -0,0 +1,6 @@
+﻿# Krzaq.Extensions.Expression
+Extension adds few method to Linq expressions.
+
+## v1.0.0
+Added:
+* `string GetMemberName<T>(this Expression<Func<T, object>> expression)`
