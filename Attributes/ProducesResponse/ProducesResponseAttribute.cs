359e478d0cd9a5b2db4011a4668f5ccbbf1dfb2c
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2026-08-23 22:15:12 +0200

    Added Krzaq.Attributes.ProducesResponse

---
+++ Attributes/ProducesResponse/ProducesResponseAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Krzaq.Attributes.ProducesResponse
+{
+    public class ProducesResponseAttribute(HttpStatusCode httpStatus)
+        : ProducesResponseTypeAttribute((int)httpStatus)
+    {
+    }
+
+    public class ProducesResponseAttribute<TResponse>(HttpStatusCode httpStatus)
+        : ProducesResponseTypeAttribute(typeof(TResponse), (int)httpStatus)
+    {
+    }
+}
---
+++ Attributes/ProducesResponse/README.md
@@ -0,0 +1,5 @@
+﻿# Krzaq.Attributes.ProducesResponse
+Attribute that specifies http status for produced responses.
+
+## v1.0.0
+* Added attribute class
